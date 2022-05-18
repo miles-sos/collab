46be8f59589a8915f9df5d4f945e917357db8071
miles-sos <frank.ebuzoeme@gmail.com>
2022-05-18 01:15:17 +0100

    created the home route

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mysql = require('mysql');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from sidehustle');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port:${PORT}`);
+});
